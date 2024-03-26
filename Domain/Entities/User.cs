39283229228f7052019388ab060c1ba97b8ddb4b
Valentino Gechev <88308767+vgechev@users.noreply.github.com>
2024-03-26 21:40:06 +0200

    Add [Required] attribute to email and password

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -3,7 +3,11 @@
 	public class User(string email, string password)
 	{
 		public int Id { get; set; }
+
+		[Required]
 		public string Email { get; set; } = email;
+
+		[Required]
 		public string Password { get; set; } = password;
 	}
 }
